3bececc7e673757b19c76e2258e789a32facbcbf
Milja <dmikr002@edu.xamk.fi>
2022-01-29 11:31:10 +0200

    Rooms and Test Map
    Added models for rooms, some kind of playable character and also test map.

--- 2nd Person Horror/Assets/SCRIPTS/TEST_character_movement.cs
+++ 2nd Person Horror/Assets/SCRIPTS/TEST_character_movement.cs
@@ -4,12 +4,93 @@ using UnityEngine;
 
 public class TEST_character_movement : MonoBehaviour
 {
-    public float speed = 30f;
+    Rigidbody playerRB;
 
-    private void Update()
+    [Header("Muuttujat")]
+    [SerializeField] float moveSpeed = 5f;
+
+
+    // Vectorit
+    Vector3 moveDirection;
+    Vector3 sideDirection;
+
+    // Boolit
+    private bool movingForward;
+    private bool movingBack;
+    private bool movingLeft;
+    private bool movingRight;
+
+    void Start()
     {
-        var horizontal = Input.GetAxis("Horizontal");
-        var vertical = Input.GetAxis("Vertical");
-        transform.Translate(new Vector3(horizontal, 0, vertical) * (speed * Time.deltaTime));
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        CheckMoveInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void CheckMoveInput()
+    {
+
+        #region Movement Input Handling
+
+        // Checks if Player is moving forward
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            movingForward = true;
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            movingForward = false;
+        }
+        // Checks if Player is moving back, and so on...
+        if (Input.GetKeyDown(KeyCode.S))
+        { movingBack = true; }
+        if (Input.GetKeyUp(KeyCode.S))
+        { movingBack = false; }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        { movingLeft = true; }
+        if (Input.GetKeyUp(KeyCode.A))
+        { movingLeft = false; }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        { movingRight = true; }
+        if (Input.GetKeyUp(KeyCode.D))
+        { movingRight = false; }
+
+
+        #endregion
+    }
+
+    private void Move()
+    {
+        moveDirection = gameObject.transform.forward;
+        sideDirection = gameObject.transform.right;
+
+        if (movingForward)
+        {
+            playerRB.AddForce(moveDirection * moveSpeed * Time.deltaTime, ForceMode.Impulse);
+        }
+        if (movingBack)
+        {
+            playerRB.AddForce(-moveDirection * moveSpeed * Time.deltaTime, ForceMode.Impulse);
+        }
+
+        if (movingLeft)
+        {
+            playerRB.AddForce(-sideDirection * moveSpeed * Time.deltaTime, ForceMode.Impulse);
+        }
+        if (movingRight)
+        {
+            playerRB.AddForce(sideDirection * moveSpeed * Time.deltaTime, ForceMode.Impulse);
+        }
     }
 }
