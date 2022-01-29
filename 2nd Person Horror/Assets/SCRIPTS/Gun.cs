7a2797cb8359b0cce051b396a89f5280c5d3fcb6
Milja <dmikr002@edu.xamk.fi>
2022-01-29 15:19:34 +0200

    Basic Gun Cycle
    How does the charger room work etc. Simple text functionality to inform player.

--- 2nd Person Horror/Assets/SCRIPTS/Gun.cs
+++ 2nd Person Horror/Assets/SCRIPTS/Gun.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
 
-    public bool isLoaded = false;
+    public bool canBeUsed = false;
+
     public GameObject projectile;
 
     // Update is called once per frame
     void Update()
     {
-        if (isLoaded && Input.GetMouseButtonDown(0))
+
+        //If player decides to shoot.
+        if (canBeUsed && Input.GetMouseButtonDown(0))
         {
-            Debug.Log("Gun is used");
             Instantiate(projectile, transform.position, Quaternion.identity);
-            isLoaded = false;
+            canBeUsed = false;
         }
     }
+
 }
--- 2nd Person Horror/Assets/SCRIPTS/Player_Charger_Room.cs
+++ 2nd Person Horror/Assets/SCRIPTS/Player_Charger_Room.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class Player_Charger_Room : MonoBehaviour
 {
+    //whenever you arrive in charger room this will be true.
+    public bool canCharge = false;
 
-    bool canCharge = false;
+    //whenever you leave your gun in the charge station this will be true.
+    public bool thereIsGun = false;
+
+    //gun can be obtained.
+    public bool gunIsReady = false;
+
+    //timer variables.
+    private static int maxTime = 5;
+    public int timer;
 
     //getting stuff from Gun-script.
-    //Gun gunLoaded = GetComponent<Gun>();
-    //bool isLoaded = gunLoaded.isLoaded;
-
     Gun gun;
     bool gunLoaded;
 
@@ -20,21 +27,24 @@ public class Player_Charger_Room : MonoBehaviour
 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (canCharge && Input.GetKeyDown("space"))
+        if (canCharge && !thereIsGun && Input.GetKeyDown("space"))
         {
-            gunLoaded = gameObject.GetComponent<Gun>().isLoaded;
+            gunLoaded = gameObject.GetComponent<Gun>().canBeUsed;
             if (!gunLoaded)
             {
-                Debug.Log("Gun is charging");
-                gameObject.GetComponent<Gun>().isLoaded = true;
+                thereIsGun = true;            
+                StartCoroutine("Countdown");
             }
-            else
-            {
-                Debug.Log("Gun is already charged!");
-            }
+        }
+
+        //if everything is fine make gun usable.
+        if (gunIsReady && canCharge && Input.GetKeyDown("space"))
+        {
+            gameObject.GetComponent<Gun>().canBeUsed = true;
+            thereIsGun = false;
+            gunIsReady = false;
         }
     }
 
@@ -54,6 +64,21 @@ public class Player_Charger_Room : MonoBehaviour
         {
             canCharge = false;
         }
+    }
+
+    //countdown.
+    private IEnumerator Countdown()
+    {
+        timer = maxTime;
+        while (timer > 0)
+        {
+
+            timer -= 1;
+            yield return new WaitForSeconds(1);
+        }
+        timer = maxTime;
+        gunIsReady = true;
+        //after countdown.
     }
 
 }
