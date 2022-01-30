82a6210974b466c6c5ec56ff022fd809d55e302d
MiksuMaker <91052332+MiksuMaker@users.noreply.github.com>
2022-01-30 16:47:19 +0200

    Audio works

--- 2nd Person Horror/Assets/AudioManager.cs
+++ 2nd Person Horror/Assets/AudioManager.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] AudioClip monsterSawYouSound;
 
-    // Update is called once per frame
-    void Update()
+    public void MonsterSawYou()
     {
-        
+        AudioSource.PlayClipAtPoint(monsterSawYouSound, FindObjectOfType<TEST_character_movement>().gameObject.transform.position);
     }
 }
