00d2864e411ac984b156c47842f51280e77f8bcc
Milja <dmikr002@edu.xamk.fi>
2022-01-30 16:23:12 +0200

    Something again
    dapkdslf

--- 2nd Person Horror/Assets/SCRIPTS/Monster/AttackScript.cs
+++ 2nd Person Horror/Assets/SCRIPTS/Monster/AttackScript.cs
@@ -6,14 +6,15 @@ public class AttackScript : MonoBehaviour
 {
     //[SerializeField] 
     GameObject gameManager;
+
     private void OnCollisionEnter(Collision collision)
     {
+            Debug.Log("Player is DEAD");
 
         if (collision.gameObject.CompareTag("Player"))
         {
             gameManager = GameObject.Find("Game Manager");
             gameManager.GetComponent<GameOver>().playerDead = true;
-            Debug.Log("Player is DEAD");
         }
     }
 }
