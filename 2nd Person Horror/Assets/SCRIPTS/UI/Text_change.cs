5d871b39b1638d26c4286dc9641f840cfc84de52
Milja <dmikr002@edu.xamk.fi>
2022-01-30 11:28:36 +0200

    New font
    For everyone

--- 2nd Person Horror/Assets/SCRIPTS/UI/Text_change.cs
+++ 2nd Person Horror/Assets/SCRIPTS/UI/Text_change.cs
@@ -20,30 +20,30 @@ public class Text_change : MonoBehaviour
         
         //When gun is ready
         if (player.GetComponent<Gun>().canBeUsed){
-            myText.text = "Gun is ready to use. Press left mouse button.";
+            myText.text = "Gun is ready to use.\nPress LEFT MOUSE BUTTON to shoot.";
         }
         
         //When gun is ready but you dont have it yet.
         else if (player.GetComponent<Player_Charger_Room>().gunIsReady)
         {
-            myText.text = "Gun is ready! Go to charger & press space.";
+            myText.text = "Your gun is ready.\nGo to the charging station.\nress SPACE to obtain your gun.";
         }
 
         //When gun is in charger.
         else if (player.GetComponent<Player_Charger_Room>().thereIsGun)
         {
-            myText.text = "Gun is charging! "+ player.GetComponent<Player_Charger_Room>().timer + " sec.";
+            myText.text = "Your gun is charging. "+ player.GetComponent<Player_Charger_Room>().timer + " sec. left.";
         }        
         
         //When you are in a charger room. 
         else if (player.GetComponent<Player_Charger_Room>().canCharge)
         {
-            myText.text = "You can charge your gun by pressing space.";
+            myText.text = "Charging station.\nYou can charge your gun by pressing SPACE.";
         }
 
         else
         {
-            myText.text = "Gun is Empty.";
+            myText.text = "Gun is Empty.\nFind a charging station to reload it.";
         }
             
     }
