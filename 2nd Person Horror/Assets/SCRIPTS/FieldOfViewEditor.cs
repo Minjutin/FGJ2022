59f0fabd9bf584cc570b2bacde647fda928172df
MiksuMaker <91052332+MiksuMaker@users.noreply.github.com>
2022-02-02 23:16:27 +0200

    Commented out the FOV Editor Script
    But it's still there!

--- 2nd Person Horror/Assets/SCRIPTS/FieldOfViewEditor.cs
+++ 2nd Person Horror/Assets/SCRIPTS/FieldOfViewEditor.cs
@@ -2,26 +2,40 @@ using UnityEngine;
 using System.Collections;
 using UnityEditor;
 
-[CustomEditor(typeof(FieldOfView))]
-public class FieldOfViewEditor : Editor
+// HUOMIO !!!
+
+// All the useful code has been commented out
+
+// This is because the game can't be built with this Editor code in it
+//(for some frikking reason)
+
+// --> Comment it out and comment out the "fake" stuff to re-activate
+
+// ( Ctrl + K, Ctrl + C			-to comment out large areas
+// ( Ctrl + K, Ctrl + U			-to uncomment out large areas
+
+
+//[CustomEditor(typeof(FieldOfView))]
+//public class FieldOfViewEditor : Editor
+public class FieldOfViewEditor : MonoBehaviour	// <-- FAKE! REMOVE!
 {
 
-	void OnSceneGUI()
-	{
-		FieldOfView fow = (FieldOfView)target;
-		Handles.color = Color.white;
-		Handles.DrawWireArc(fow.transform.position, Vector3.up, Vector3.forward, 360, fow.viewRadius);
-		Vector3 viewAngleA = fow.DirFromAngle(-fow.viewAngle / 2, false);
-		Vector3 viewAngleB = fow.DirFromAngle(fow.viewAngle / 2, false);
+	//void OnSceneGUI()
+	//{
+	//	FieldOfView fow = (FieldOfView)target;
+	//	Handles.color = Color.white;
+	//	Handles.DrawWireArc(fow.transform.position, Vector3.up, Vector3.forward, 360, fow.viewRadius);
+	//	Vector3 viewAngleA = fow.DirFromAngle(-fow.viewAngle / 2, false);
+	//	Vector3 viewAngleB = fow.DirFromAngle(fow.viewAngle / 2, false);
 
-		Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
-		Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
+	//	Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
+	//	Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
 
-		Handles.color = Color.red;
-		foreach (Transform visibleTarget in fow.visibleTargets)
-		{
-			Handles.DrawLine(fow.transform.position, visibleTarget.position);
-		}
-	}
+	//	Handles.color = Color.red;
+	//	foreach (Transform visibleTarget in fow.visibleTargets)
+	//	{
+	//		Handles.DrawLine(fow.transform.position, visibleTarget.position);
+	//	}
+	//}
 
 }
