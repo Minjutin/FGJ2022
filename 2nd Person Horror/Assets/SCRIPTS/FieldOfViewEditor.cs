efe9c235f4b7da98bc2229fecc3276cb306df6ce
MiksuMaker <91052332+MiksuMaker@users.noreply.github.com>
2022-02-03 22:46:50 +0200

    Update FieldOfViewEditor.cs

--- 2nd Person Horror/Assets/SCRIPTS/FieldOfViewEditor.cs
+++ 2nd Person Horror/Assets/SCRIPTS/FieldOfViewEditor.cs
@@ -15,27 +15,27 @@ using UnityEditor;
 // ( Ctrl + K, Ctrl + U			-to uncomment out large areas
 
 
-[CustomEditor(typeof(FieldOfView))]
-public class FieldOfViewEditor : Editor
-//public class FieldOfViewEditor : MonoBehaviour	// <-- FAKE! REMOVE!
+//[CustomEditor(typeof(FieldOfView))]
+//public class FieldOfViewEditor : Editor
+public class FieldOfViewEditor : MonoBehaviour	// <-- FAKE! REMOVE!
 {
 
-    void OnSceneGUI()
-    {
-        FieldOfView fow = (FieldOfView)target;
-        Handles.color = Color.white;
-        Handles.DrawWireArc(fow.transform.position, Vector3.up, Vector3.forward, 360, fow.viewRadius);
-        Vector3 viewAngleA = fow.DirFromAngle(-fow.viewAngle / 2, false);
-        Vector3 viewAngleB = fow.DirFromAngle(fow.viewAngle / 2, false);
+    //void OnSceneGUI()
+    //{
+    //    FieldOfView fow = (FieldOfView)target;
+    //    Handles.color = Color.white;
+    //    Handles.DrawWireArc(fow.transform.position, Vector3.up, Vector3.forward, 360, fow.viewRadius);
+    //    Vector3 viewAngleA = fow.DirFromAngle(-fow.viewAngle / 2, false);
+    //    Vector3 viewAngleB = fow.DirFromAngle(fow.viewAngle / 2, false);
 
-        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
-        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
+    //    Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
+    //    Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
 
-        Handles.color = Color.red;
-        foreach (Transform visibleTarget in fow.visibleTargets)
-        {
-            Handles.DrawLine(fow.transform.position, visibleTarget.position);
-        }
-    }
+    //    Handles.color = Color.red;
+    //    foreach (Transform visibleTarget in fow.visibleTargets)
+    //    {
+    //        Handles.DrawLine(fow.transform.position, visibleTarget.position);
+    //    }
+    //}
 
 }
